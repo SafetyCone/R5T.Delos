e5f6b1d6de4ea97037ab3dde8aca496cf0d4fcc7
MinexAutomation <minex.automation@gmail.com>
2019-12-10 10:27:05 -0800

    Added machine-location typed-string type.

---
+++ source/R5T.Delos/Code/Classes/MachineLocation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using R5T.Stagira;
+
+
+namespace R5T.Delos
+{
+    public class MachineLocation : TypedString
+    {
+        public MachineLocation(string value)
+            : base(value)
+        {
+        }
+    }
+}
